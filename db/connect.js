4c74621555378be91e8c7c314c6c63b4a98999d8
sadik <po480373@gmai.com>
2022-09-15 23:30:47 +0600

    vercel upload4

--- db/connect.js
+++ db/connect.js
@@ -1,9 +1,10 @@
 const colors = require("colors");
 const mongoose = require("mongoose");
-// mongodb+srv://sadik:VFYAjL1PjzqzAQLH@cluster0.97alsyr.mongodb.net/bookmeup
 exports.connect = async () => {
   try {
-    await mongoose.connect(process.env.DB);
+    await mongoose.connect(
+      "mongodb+srv://sadik:sadik123@cluster0.ljrbn8h.mongodb.net/bookmeup?retryWrites=true&w=majority"
+    );
     console.log("connected to db".yellow.bold);
   } catch (error) {
     throw error;
